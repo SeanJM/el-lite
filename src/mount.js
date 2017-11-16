e48e031b70ca028ceada837f7d80b8a60b6debaa
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-11-16 18:55:01 -0500

    Fixed bubbling

--- src/mount.js
+++ src/mount.js
@@ -2,11 +2,9 @@ module.exports = function mount(node) {
   const children = node.childNodes;
 
   if (document.body.contains(node)) {
-    if (children.length === 0) {
-      node.dispatchEvent(
-        new Event("__mount")
-      );
-    }
+    node.dispatchEvent(
+      new Event("__mount", { bubbles: false })
+    );
 
     for (var i = 0, n = children.length; i < n; i++) {
       mount(children[i]);
--- src/unmount.js
+++ src/unmount.js
@@ -2,7 +2,7 @@ module.exports = function unmount(node) {
   const children = node.childNodes;
   if (children.length === 0) {
     node.dispatchEvent(
-      new Event("__unmount")
+      new Event("__unmount", { bubbles: false })
     );
   }
   for (var i = 0, n = children.length; i < n; i++) {
