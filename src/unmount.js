d84cde8309b1b9e1cd661b0bef5f9ddf22311cf2
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-11-16 22:04:24 -0500

    Fixed unmount

--- src/unmount.js
+++ src/unmount.js
@@ -1,10 +1,10 @@
 module.exports = function unmount(node) {
   const children = node.childNodes;
-  if (children.length === 0) {
-    node.dispatchEvent(
-      new Event("__unmount", { bubbles: false })
-    );
-  }
+
+  node.dispatchEvent(
+    new Event("__unmount", { bubbles: false })
+  );
+
   for (var i = 0, n = children.length; i < n; i++) {
     unmount(children[i]);
   }
